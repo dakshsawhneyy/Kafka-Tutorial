8ad845ddf4875c921bac332c01add25b7b7191c0
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-29 16:52:33 +0000

    Commented consumer.js

--- consumer.js
+++ consumer.js
@@ -1,9 +1,9 @@
 const { kafka } = require('./client')
-const group = process.argv[2];
+const group = process.argv[2];  // takes group name as second parameter from CLI
 
 
 async function init() {
-    const consumer = kafka.consumer({ groupId: group })
+    const consumer = kafka.consumer({ groupId: group })     //  consumer is part of the group you specified
     await consumer.connect();
 
     await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true })   // message shuru se chaiye; from beginning
