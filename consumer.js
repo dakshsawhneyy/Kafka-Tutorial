8a178d5cd42f25035103bac6594169db3b6f3c69
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-29 15:05:31 +0000

    groups and updated consumer producer with multiple inputs

--- consumer.js
+++ consumer.js
@@ -1,14 +1,16 @@
 const { kafka } = require('./client')
+const group = process.argv[2];
+
 
 async function init() {
-    const consumer = kafka.consumer({ groupId: "user-1" })
+    const consumer = kafka.consumer({ groupId: group })
     await consumer.connect();
 
     await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true })   // message shuru se chaiye; from beginning
 
     await consumer.run({
         eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
-            console.log(`[${topic}]: PART:${partition}:`, message.value.toString())
+            console.log(`${group}: [${topic}]: PART:${partition}:`, message.value.toString())
         }
     })
 
--- producer.js
+++ producer.js
@@ -1,4 +1,11 @@
 const { kafka } = require('./client')
+const readline = require('readline')
+
+// Taking Multiple Inputs from User
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 async function init(){
     const producer = kafka.producer();
@@ -7,23 +14,34 @@ async function init(){
     await producer.connect();
     console.log('Producer Connected');
 
-    console.log('Sending Message [rider-updates]');
-    await producer.send({
-        topic: 'rider-updates',
-        messages: [
-            {
-                partition: 0,
-                key: 'location-update', 
-                value: JSON.stringify({name: 'Tony Stark', loc: 'NORTH'}), // need json.Stringify because without it it will send JS Object which is not required
-            },   
-        ]
-    })
-    console.log('Message Sent Successfully [rider-updates]');
+    // Ask for various inputs from the user
+    rl.setPrompt('>');
+    rl.prompt();
 
-    // Disconnect Producer
-    console.log("Disconnecting Producer.......")
-    await producer.disconnect();
-    console.log('Producer Disconnected Successfully');
+    // Whenever user prompts
+    rl.on('line', async function(line) {
+        const [riderName, location] = line.split(' ');
+        
+        console.log('Sending Message [rider-updates]');
+        await producer.send({
+            topic: 'rider-updates',
+            messages: [
+                {
+                    partition: location.toLowerCase() == 'north' ? 0 : 1,   // for north -> use 0 else 1
+                    key: 'location-update', 
+                    value: JSON.stringify({name: riderName, loc: location}), // need json.Stringify because without it it will send JS Object which is not required
+                },   
+            ]
+        })
+        console.log('Message Sent Successfully [rider-updates]');
+    
+    }).on('close', async() => {
+        // Disconnect Producer
+        console.log("Disconnecting Producer.......")
+        await producer.disconnect();
+        console.log('Producer Disconnected Successfully');
+    })
+    
 }
 
 init();
